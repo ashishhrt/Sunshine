5b9216951c8141c8ce4f9dce53eeb2a1f6f655d8
crockrox <ashishhrt@gmail.com>
2016-06-29 12:06:09 +0530

    details fragement menu added

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -3,12 +3,17 @@ package com.example.android.sunshine.app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.NavUtils;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,6 +45,7 @@ public class DetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.detail, menu);
+
         return true;
     }
 
@@ -50,16 +56,16 @@ public class DetailActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            //open settings activity
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
-        }
 
-        //up navigation to parent activity handler
+
         switch(id){
+            case R.id.action_settings:
+                //open settings activity
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            //up navigation to parent activity handler
             case android.R.id.home:
                 //when this activity is invokable from within this app
                 NavUtils.navigateUpFromSameTask(this);
@@ -74,7 +80,13 @@ public class DetailActivity extends AppCompatActivity {
      */
     public static class DetailFragment extends Fragment {
 
+        private static final String LOG_TAG = DetailFragment.class.getSimpleName();
+        private static final String FORECAST_SHARE_HASTAG = " #SunshineApp";
+        private ShareActionProvider shareActionProvider = null;
+        private String mforecastString = "";
+
         public DetailFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -86,12 +98,56 @@ public class DetailActivity extends AppCompatActivity {
             Intent intent = getActivity().getIntent();
             if(intent != null && intent.hasExtra(ForecastFragment.EXTRA_FORECAST)){
 
+                mforecastString = intent.getStringExtra(ForecastFragment.EXTRA_FORECAST);
+
                 TextView textView = (TextView) rootView.findViewById(R.id.detail_text);
-                textView.setText(intent.getStringExtra(ForecastFragment.EXTRA_FORECAST));
+                textView.setText(mforecastString);
 
             }
 
             return rootView;
         }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+
+            inflater.inflate(R.menu.detailsfragment, menu);
+
+            //make share menu visible
+            MenuItem item = menu.findItem(R.id.action_share);
+            //get share action provider
+            shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
+            //call createShareIntent() to attach an intent with shareActionProvider
+            createShareIntent();
+        }
+
+        //call it to update share intent
+        private void setShareIntent(Intent shareIntent){
+
+            if(shareIntent != null) {
+                shareActionProvider.setShareIntent(shareIntent);
+            }
+            else{
+                Log.e(LOG_TAG, "Share Action Provider is null ?");
+            }
+
+        }
+
+        //call to create and attach an intent with shareActionProvider
+        private void createShareIntent(){
+
+            //create share intent and call setShareIntent
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("text/*");
+            //get data to share
+            String locationPin = "\nLocation PIN: "+PreferenceManager.getDefaultSharedPreferences(getContext()).getString(getString(R.string.key_location), getString(R.string.default_location_value));
+            String weather = mforecastString + FORECAST_SHARE_HASTAG + locationPin;
+            intent.putExtra(Intent.EXTRA_TEXT, weather);
+            setShareIntent(intent);
+
+        }
+
     }
 }
--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,6 +43,25 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
 
             return true;
+        }
+        else if (id == R.id.action_view_map) {
+            //view location on map
+            String location = "geo:0,0?q=";
+            location += PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.key_location), getString(R.string.default_location_value));
+            //create location Uri
+            Uri loactionUri = Uri.parse(location);
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(loactionUri);
+
+            if(intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+            else {
+                String message = "Install Google maps !";
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+            }
+
+
         }
 
         return super.onOptionsItemSelected(item);
